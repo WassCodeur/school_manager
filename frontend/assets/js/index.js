c51774967855e8987df1487479e3a6dbc7e12c29
WassCodeur <wachioubouraima56@gmail.com>
2025-02-02 17:54:56 +0000

    Update contract address and fetch ABI dynamically; enhance error handling in frontend

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -1,158 +1,22 @@
 // const contractAddress = '0xDFC762D7ee023AC20273AFE7D7257e5a982a6984';
 const contractAddress = '0x45f30aFEc1e54F92D73985178F2DD0e30Ccbdb89';
 
-const contractABI = [
-    {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": false,
-                "internalType": "string",
-                "name": "name",
-                "type": "string"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "rollNumber",
-                "type": "uint256"
-            },
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "rollDate",
-                "type": "uint256"
-            }
-        ],
-        "name": "StudentRegistered",
-        "type": "event"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": false,
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "studentRemoved",
-        "type": "event"
-    },
-    {
-        "inputs": [],
-        "name": "admin",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            { "internalType": "uint256", "name": "rollNumber", "type": "uint256" }
-        ],
-        "name": "getStudentById",
-        "outputs": [
-            {
-                "components": [
-                    { "internalType": "string", "name": "name", "type": "string" },
-                    { "internalType": "uint256", "name": "rollNumber", "type": "uint256" },
-                    { "internalType": "uint256", "name": "rollDate", "type": "uint256" }
-                ],
-                "internalType": "struct SchoolManager.Student",
-                "name": "",
-                "type": "tuple"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "studentName",
-                "type": "string"
-            },
-            {
-                "internalType": "uint256",
-                "name": "rollNumber",
-                "type": "uint256"
-            }
-        ],
-        "name": "registerStudent",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "rollNumber",
-                "type": "uint256"
-            }
-        ],
-        "name": "removeStudent",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "students",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "name",
-                "type": "string"
-            },
-            {
-                "internalType": "uint256",
-                "name": "rollNumber",
-                "type": "uint256"
-            },
-            {
-                "internalType": "uint256",
-                "name": "rollDate",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }
-];
-
-
-// fetch('./assets/abis/abi.json')
-//     .then((response) => response.json())
-//     .then((json) => console.log(json));
+let contractABI;
 
 let web3;
 let schoolManager;
 
+
 window.addEventListener('load', async () => {
     if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        await window.ethereum.enable();
-        schoolManager = new web3.eth.Contract(contractABI, contractAddress);
+        try {
+            web3 = new Web3(window.ethereum);
+            await window.ethereum.enable();
+            contractABI = await fetch('./assets/abis/abi.json').then((response) => response.json());
+            schoolManager = new web3.eth.Contract(contractABI, contractAddress);
+        } catch (error) {
+            alert('Error: ' + error);
+        }
     } else {
         alert('Please install MetaMask!');
     }
@@ -182,10 +46,7 @@ document.getElementById('getStudentForm').addEventListener('submit', async (even
     try {
         event.preventDefault();
         const rollNumber = document.getElementById('getRollNumber').value;
-        // const rollNumberBN = BigInt(Number(rollNumber / 10) * 10);
         const rollNumberStr = rollNumber.toString();
-        console.log(rollNumberStr)
-        console.log(await schoolManager.methods.getStudentById(rollNumberStr).call())
 
         const student = await schoolManager.methods.getStudentById(rollNumberStr).call();
         const studentRoolNumber = parseInt(student.rollNumber);
